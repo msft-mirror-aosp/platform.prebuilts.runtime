b9e344ecf28a2e189c99c462d66add4a0250a0f8
Nicolas Geoffray <ngeoffray@google.com>
2020-06-30 08:58:04 +0100

    Update mainline prebuilts to build 6640873.
    Taken from branch aosp-master.
    
    Change-Id: I0a0dfa3852db30ad31dd0bc6d0582d1d9d185594

---
+++ mainline/i18n/sdk/sdk_library/public/i18n.module.intra.core.api_stub_sources/android/icu/util/TimeZoneRule.java
@@ -0,0 +1,24 @@
+/* GENERATED SOURCE. DO NOT MODIFY. */
+// ? 2016 and later: Unicode, Inc. and others.
+// License & terms of use: http://www.unicode.org/copyright.html#License
+/*
+ *******************************************************************************
+ * Copyright (C) 2007-2016, International Business Machines Corporation and    *
+ * others. All Rights Reserved.                                                *
+ *******************************************************************************
+ */
+
+package android.icu.util;
+
+@SuppressWarnings({"unchecked", "deprecation", "all"})
+public abstract class TimeZoneRule implements java.io.Serializable {
+
+TimeZoneRule() { throw new RuntimeException("Stub!"); }
+
+public int getRawOffset() { throw new RuntimeException("Stub!"); }
+
+public int getDSTSavings() { throw new RuntimeException("Stub!"); }
+
+public java.lang.String toString() { throw new RuntimeException("Stub!"); }
+}
+
---
+++ mainline/i18n/sdk/sdk_library/public/stable.i18n.module.platform.api_stub_sources/android/icu/text/RelativeDateTimeFormatter.java
@@ -0,0 +1,109 @@
+/* GENERATED SOURCE. DO NOT MODIFY. */
+// ? 2016 and later: Unicode, Inc. and others.
+// License & terms of use: http://www.unicode.org/copyright.html#License
+/*
+ *******************************************************************************
+ * Copyright (C) 2013-2016, International Business Machines Corporation and
+ * others. All Rights Reserved.
+ *******************************************************************************
+ */
+
+package android.icu.text;
+
+@SuppressWarnings({"unchecked", "deprecation", "all"})
+public final class RelativeDateTimeFormatter {
+
+private RelativeDateTimeFormatter() { throw new RuntimeException("Stub!"); }
+
+public static android.icu.text.RelativeDateTimeFormatter getInstance() { throw new RuntimeException("Stub!"); }
+
+public static android.icu.text.RelativeDateTimeFormatter getInstance(android.icu.util.ULocale locale) { throw new RuntimeException("Stub!"); }
+
+public static android.icu.text.RelativeDateTimeFormatter getInstance(java.util.Locale locale) { throw new RuntimeException("Stub!"); }
+
+public static android.icu.text.RelativeDateTimeFormatter getInstance(android.icu.util.ULocale locale, android.icu.text.NumberFormat nf) { throw new RuntimeException("Stub!"); }
+
+public static android.icu.text.RelativeDateTimeFormatter getInstance(android.icu.util.ULocale locale, android.icu.text.NumberFormat nf, android.icu.text.RelativeDateTimeFormatter.Style style, android.icu.text.DisplayContext capitalizationContext) { throw new RuntimeException("Stub!"); }
+
+public static android.icu.text.RelativeDateTimeFormatter getInstance(java.util.Locale locale, android.icu.text.NumberFormat nf) { throw new RuntimeException("Stub!"); }
+
+public java.lang.String format(double quantity, android.icu.text.RelativeDateTimeFormatter.Direction direction, android.icu.text.RelativeDateTimeFormatter.RelativeUnit unit) { throw new RuntimeException("Stub!"); }
+
+public java.lang.String formatNumeric(double offset, android.icu.text.RelativeDateTimeFormatter.RelativeDateTimeUnit unit) { throw new RuntimeException("Stub!"); }
+
+public java.lang.String format(android.icu.text.RelativeDateTimeFormatter.Direction direction, android.icu.text.RelativeDateTimeFormatter.AbsoluteUnit unit) { throw new RuntimeException("Stub!"); }
+
+public java.lang.String format(double offset, android.icu.text.RelativeDateTimeFormatter.RelativeDateTimeUnit unit) { throw new RuntimeException("Stub!"); }
+
+public java.lang.String combineDateAndTime(java.lang.String relativeDateString, java.lang.String timeString) { throw new RuntimeException("Stub!"); }
+
+public android.icu.text.NumberFormat getNumberFormat() { throw new RuntimeException("Stub!"); }
+
+public android.icu.text.DisplayContext getCapitalizationContext() { throw new RuntimeException("Stub!"); }
+
+public android.icu.text.RelativeDateTimeFormatter.Style getFormatStyle() { throw new RuntimeException("Stub!"); }
+@SuppressWarnings({"unchecked", "deprecation", "all"})
+public enum AbsoluteUnit {
+SUNDAY,
+MONDAY,
+TUESDAY,
+WEDNESDAY,
+THURSDAY,
+FRIDAY,
+SATURDAY,
+DAY,
+WEEK,
+MONTH,
+YEAR,
+NOW;
+}
+
+@SuppressWarnings({"unchecked", "deprecation", "all"})
+public enum Direction {
+LAST_2,
+LAST,
+THIS,
+NEXT,
+NEXT_2,
+PLAIN;
+}
+
+@SuppressWarnings({"unchecked", "deprecation", "all"})
+public enum RelativeDateTimeUnit {
+YEAR,
+QUARTER,
+MONTH,
+WEEK,
+DAY,
+HOUR,
+MINUTE,
+SECOND,
+SUNDAY,
+MONDAY,
+TUESDAY,
+WEDNESDAY,
+THURSDAY,
+FRIDAY,
+SATURDAY;
+}
+
+@SuppressWarnings({"unchecked", "deprecation", "all"})
+public enum RelativeUnit {
+SECONDS,
+MINUTES,
+HOURS,
+DAYS,
+WEEKS,
+MONTHS,
+YEARS;
+}
+
+@SuppressWarnings({"unchecked", "deprecation", "all"})
+public enum Style {
+LONG,
+SHORT,
+NARROW;
+}
+
+}
+
